ccc25a65032068ea47788c96be02dded49b9b917
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-12-02 15:18:44 +0100

    creazione e assegnazione dello stato

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import Card from "../Card/Card"
 import posts from '../../posts.jsx'
 import Tags from '../Tags/Tags.jsx'
+import { useState } from "react"
 
 console.log(posts.published)
 
@@ -9,6 +10,7 @@ export default function Main() {
 
     const publishedPosts = posts.filter((post) => post.published)
     const tags = []
+    const [newTitle, setNewTitle] = useState('Peonie')
 
     posts.forEach(post => {
 
@@ -23,6 +25,13 @@ export default function Main() {
 
     })
 
+    function addPost(event) {
+        event.preventDefault()
+        console.log('stai aggiungendo un post')
+
+
+    }
+
 
     return (
         <main>
@@ -31,8 +40,8 @@ export default function Main() {
                     <Tags tags={tags} />
                 </div>
                 <div className="container">
-                    <form className="form" action="">
-                        <input className="formText" type="text" placeholder="Inserisci il titolo del Post" />
+                    <form onSubmit={addPost} className="form" action="">
+                        <input className="formText" type="text" placeholder="Inserisci il titolo del Post" value={newTitle} />
                         <input className="formBtn" type="submit" value={'INVIA'} />
                     </form>
                 </div>
